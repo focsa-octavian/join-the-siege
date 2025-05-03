40002a245fa790f52ed3d63e9a5b7939641f74a6
Robocto <octavian_focsa@hotmail.com>
2025-05-03 12:34:35 +0100

    add pdf reader and ocr

--- src/classifier.py
+++ src/classifier.py
@@ -1,9 +1,13 @@
 from werkzeug.datastructures import FileStorage
+import io
 
 from spellchecker import SpellChecker
+from pypdf import PdfReader
+import easyocr
+
 
 spell = SpellChecker()
-
+ocr_reader = easyocr.Reader(['en']) # model_storage_directory='~/.EasyOCR/model/', download_enabled=False
 
 def base(filename):
     if "drivers_license" in filename or "drivers_licence" in filename:
@@ -23,6 +27,33 @@ def get_spellcheck(filename):
     file_basename_correction = "_".join([spell.correction(word) for word in file_basename_list])
     return file_basename_correction
 
+def get_words(file):
+
+    filename = file.filename.lower()
+    extension = filename.rsplit('.', 1)[1].lower()
+
+    if extension == "pdf":
+        reader = PdfReader(file)
+        words = []
+
+        for page in reader.pages:
+            text_page = page.extract_text()
+            words_per_line_per_page = [line.split(" ") for line in text_page.split("\n")]
+            for line in words_per_line_per_page:
+                for word in line:
+                    words.append(word.lower())
+
+        return words
+
+    elif extension in ["png", "jpg"]:
+        result = ocr_reader.readtext(file.read())
+        words_raw = [word[-2].split(" ") for word in result]
+        words = [word.lower() for innerList in words_raw for word in innerList]
+        
+        return words
+
+    return []
+
 def classify_file(file: FileStorage):
     filename = file.filename.lower()
     # file_bytes = file.read()
@@ -41,6 +72,19 @@ def classify_file(file: FileStorage):
 
     if result:
         return result
+    
+    # 2 PDF Reader & OCR
+
+    words = get_words(file)
+
+    if set(['bank', 'statement']) <= set(words):
+        return "bank_statement"
+    
+    if set(["driving", "driver"]).intersection(words) and set(["licence", "license"]).intersection(words):
+        return "drivers_licence"
+
+    if "invoice" in words:
+        return "invoice"
 
     return "unknown file"
 
