5da83c3c5e6d9e4ecacb16aea2bbf5b9be1cb37f
Robocto <octavian_focsa@hotmail.com>
2025-05-06 11:57:54 +0100

    add test (commented)

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,6 @@
 from io import BytesIO
+import os
+import random
 
 import pytest
 from src.app import app, allowed_file
@@ -36,3 +38,59 @@ def test_success(client, mocker):
     response = client.post('/classify_file', data=data, content_type='multipart/form-data')
     assert response.status_code == 200
     assert response.get_json() == {"file_class": "test_class"}
+
+# @pytest.mark.parametrize("filename, classification", [
+#     ("bank_statement_1.pdf", "bank_statement"),
+#     ("bank_statement_2.pdf", "bank_statement"),
+#     ("bank_statement_3.pdf", "bank_statement"),
+#     ("drivers_license_1.jpg", "drivers_licence"),
+#     ("drivers_licence_2.jpg", "drivers_licence"),
+#     ("drivers_license_3.jpg", "drivers_licence"),
+#     ("invoice_1.pdf", "invoice"),
+#     ("invoice_2.pdf", "invoice"),
+#     ("invoice_3.pdf", "invoice"),
+# ])
+# def test_base_w_spellcheck(client, filename, classification):
+
+#     dir = "files"
+#     filepath = os.path.join(dir, filename)
+    
+#     with open(filepath, 'rb') as fh:
+#         file = BytesIO(fh.read())
+
+#     # basename, extension = filename.split(".")
+#     # idx = random.randint(0, len(basename) - 1)
+#     # basename = basename[:idx] + "_" + basename[idx+1:]
+#     # filename = str(".".join([basename, extension]))
+
+#     data = {'file': (file, filename)}
+#     response = client.post('/classify_file', data=data, content_type='multipart/form-data')
+#     assert response.status_code == 200
+#     assert response.get_json() == {"file_class": classification}
+
+# @pytest.mark.parametrize("filename, classification", [
+#     ("bank_statement_1.pdf", "bank_statement"),
+#     ("bank_statement_2.pdf", "bank_statement"),
+#     ("bank_statement_3.pdf", "bank_statement"),
+#     ("drivers_license_1.jpg", "drivers_licence"),
+#     ("drivers_licence_2.jpg", "drivers_licence"),
+#     ("drivers_license_3.jpg", "drivers_licence"),
+#     ("invoice_1.pdf", "invoice"),
+#     ("invoice_2.pdf", "invoice"),
+#     ("invoice_3.pdf", "unknown file"),
+# ])
+# def test_(client, filename, classification):
+
+#     dir = "files"
+#     filepath = os.path.join(dir, filename)
+    
+#     with open(filepath, 'rb') as fh:
+#         file = BytesIO(fh.read())
+
+#     extension = filename.split(".")[-1]
+#     filename = "test" + extension
+
+#     data = {'file': (file, filename)}
+#     response = client.post('/classify_file', data=data, content_type='multipart/form-data')
+#     assert response.status_code == 200
+#     assert response.get_json() == {"file_class": classification}
